33cd8d491ae969c960eee6abd7205d82b28da1f3
Ednilson <ednilsoncastanheira53@gmail.com>
2023-01-08 17:35:48 +0000

    Finished the Prep02 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,40 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
+    {   string letter = "";
         Console.Write("What was your grade percentage? ");
         string userGrade = Console.ReadLine();
         int intVersion = int.Parse(userGrade);
         if (intVersion >= 90)
-         {
-            Console.Write("Your grade is A. ");
-            Console.Write("Congratulations!! You passed the class. ");
+         {  
+            letter = "A";
         }
 
         else if (intVersion >= 80)
         {
-            Console.Write("Your grade is B. ");
-            Console.Write("Congratulations!! You passed the class. ");
+            letter = "B";
         }
         else if (intVersion >= 70)
         {
-            Console.Write("Your grade is C. ");
-            Console.Write("Congratulations!! You passed the class. ");
+            letter = "C";
         }
         else if (intVersion >= 60)
         {
-            Console.Write("Your grade is D. ");
-            Console.Write("Unfortunately, your grade is not enough to pass the class. Keep trying. ");
+            letter = "D";
         }
         else if (intVersion >= 50)
         {
-            Console.Write("Your grade is E. ");
-            Console.Write("Unfortunately, your grade is not enough to pass the class. Keep trying. ");
+            letter = "E";
         }
         else if (intVersion >= 40)
         {
-            Console.Write("Your grade is F. ");
-            Console.Write("Unfortunately, your grade is not enough to pass the class. Keep trying. ");
+            letter = "F";
+        }
+
+        Console.Write($"Your grade is {letter}. ");
+        if (intVersion >= 70)
+        {
+            Console.Write("Congratulations!! You passed the class. ");
+        }
+        else if (intVersion < 70)
+        {
+            Console.Write("Unfortunately, your grade is not yet high enough to pass the class. ");
         }
     }
 }
