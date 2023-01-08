6d0b7d4d7ec7063339cf96f82abaa711b32c1eeb
Ednilson <ednilsoncastanheira53@gmail.com>
2023-01-08 17:14:15 +0000

    Finished Prep02

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int intVersion = int.Parse(userGrade);
+        if (intVersion >= 90)
+         {
+            Console.Write("Your grade is A. ");
+            Console.Write("Congratulations!! You passed the class. ");
+        }
+
+        else if (intVersion >= 80)
+        {
+            Console.Write("Your grade is B. ");
+            Console.Write("Congratulations!! You passed the class. ");
+        }
+        else if (intVersion >= 70)
+        {
+            Console.Write("Your grade is C. ");
+            Console.Write("Congratulations!! You passed the class. ");
+        }
+        else if (intVersion >= 60)
+        {
+            Console.Write("Your grade is D. ");
+            Console.Write("Unfortunately, your grade is not enough to pass the class. Keep trying. ");
+        }
+        else if (intVersion >= 50)
+        {
+            Console.Write("Your grade is E. ");
+            Console.Write("Unfortunately, your grade is not enough to pass the class. Keep trying. ");
+        }
+        else if (intVersion >= 40)
+        {
+            Console.Write("Your grade is F. ");
+            Console.Write("Unfortunately, your grade is not enough to pass the class. Keep trying. ");
+        }
     }
 }
