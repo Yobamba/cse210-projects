357fdd9ba0696e897a7e97cfb58216a0800bceb8
Ednilson <ednilsoncastanheira53@gmail.com>
2023-01-18 14:01:06 +0000

    finished the week 3 prep work

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,33 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Vodafone";
+        job1._jobTitle = "Telecom Technician";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Nowo";
+        job2._jobTitle = "Software Developer";
+        job2._startYear = 2021;
+        job2._endYear = 2026;
+        
+        job1.Display();
+        job2.Display();
+
+        Resume resume1 = new Resume();
+
+        resume1._name = "Eddie Bambo";
+        resume1._job.Add(job1);
+        resume1._job.Add(job2);
+        resume1.DisplayResume();
+
+
+
+        
     }
 }
